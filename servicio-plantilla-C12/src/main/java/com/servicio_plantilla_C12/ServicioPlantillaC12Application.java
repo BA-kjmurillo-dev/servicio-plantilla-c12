69572950a4d5c57020f6d1ac7ea2408f2b7951d7
Kelvin Julian Murillo Galo <kjmurillo@bancatlan.hn>
2024-12-03 11:48:49 -0600

    Se agrego la base para la plantilla del servicio con java springboot

---
+++ servicio-plantilla-C12/src/main/java/com/servicio_plantilla_C12/ServicioPlantillaC12Application.java
@@ -0,0 +1,13 @@
+package com.servicio_plantilla_C12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioPlantillaC12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioPlantillaC12Application.class, args);
+	}
+
+}
---
+++ servicio-plantilla-C12/src/test/java/com/servicio_plantilla_C12/ServicioPlantillaC12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio_plantilla_C12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioPlantillaC12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
